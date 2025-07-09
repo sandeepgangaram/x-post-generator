a61f662d70381a3f01db6c4ecf03619de6caaff7
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2025-07-09 08:44:29 +0530

    config openai keys

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+def generate_x_post(user_input:str)->str:
+    payload = {
+        "model":"",
+        "input":""
+    }
+    
+    resonse = requests.post(
+        "https://api.openai.com/v1/responses", 
+        json=payload,
+        headers={
+            "Content-Type":"application/json",
+            "Authorization":f"Bearer {OPENAI_API_KEY}"
+        })
+
 def main():
-    print("Hello from x-post-generator!")
+    user_input = input("What you want the post to be about?")
+    x_post = generate_x_post(user_input)
+    print(x_post)
 
 
 if __name__ == "__main__":
